2aeeed3f93d340e68bce01d4c6b6212a1519dab4
Yashvardhan Baid <codechaser2002@gmail.com>
2022-02-26 19:03:52 +0000

    [not working] saving progress

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 import os
 import requests
 import json
@@ -15,7 +16,35 @@ VARIABLES
 
 """
 
-client = discord.Client()
+def get_prefix(client, message):
+    with open('./data/prefixes.json', 'r') as pref:
+        prefixes = json.load(pref)
+    return prefixes[str(message.guild.id)]
+    
+client = commands.Bot(command_prefix = get_prefix)
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+    await client.get_channel(int(os.environ['CONSOLE_CHANNEL_ID'])).send(".\nLogged in!\nOnline Now!")
+    await client.change_presence(activity=discord.Activity(type = discord.ActivityType.listening, name = db["command_key"]))
+    
+@client.event
+async def on_guild_join(guild):
+    with open('./data/prefixes.json', 'r') as pref:
+        prefixes = json.load(pref)
+    prefixes[str(guild.id)] = '$dex'
+    with open('./data/prefixes.json', 'w') as pref:
+        json.dump(prefixes, pref, indent = 4)
+
+@client.event
+async def on_guild_remove(guild):
+    with open('./data/prefixes.json', 'r') as pref:
+        prefixes = json.load(pref)
+    if str(guild.id) in prefixes.keys():
+        prefixes.pop(str(guild.id))
+    with open('./data/prefixes.json', 'w') as pref:
+        json.dump(prefixes, pref, indent = 4)
 
 def get_quote():
     response = requests.get("https://zenquotes.io/api//random")
@@ -23,14 +52,15 @@ def get_quote():
     quote = "\"" + quote_json[0]['q'] + "\" -" + quote_json[0]['a']
     return (quote)
 
+@client.command
+async def change_prefix(ctx, prefix):
+    with open('./data/prefixes.json', 'r') as pref:
+        prefixes = json.load(pref)
+    prefixes[str(ctx.guild.id)] = prefix
+    with open('./data/prefixes.json', 'w') as pref:
+        json.dump(prefixes, pref, indent = 4)
 
-@client.event
-async def on_ready():
-    print('Logged in as {0.user}'.format(client))
-    await client.get_channel(int(os.environ['CONSOLE_CHANNEL_ID'])).send(".\nLogged in!\nOnline Now!")
-    await client.change_presence(activity=discord.Activity(type = discord.ActivityType.listening, name = db["command_key"]))
-
-
+"""
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -39,11 +69,12 @@ async def on_message(message):
     
     if message.content.startswith("$dex"):
         if message.content.startswith("$dex inspire"):
-            await message.channel.send("\nOriginal message sent: \"" + message.content + "\"\n\n")
+            await message.channel.send("\nReplying to: \"" + message.content + "\" -" + message.author.mention + "\n\n")
             await message.channel.send("\n" + get_quote())
         else:
-            await message.channel.send("\nOriginal message sent: \"" + message.content + "\"\n\n")
+            await message.channel.send("\nReplying to: \"" + message.content + "\" -" + message.author.mention + "\n\n")
             await message.channel.send('\nHello, this is dex at your service!')
-
+            await client.get_channel(int(os.environ['HISTORY_CHANNEL_ID'])).send("\nMessage:\n" + message.content + "\n\nAuthor: " + str(message.author) + " | ID: " + str(message.author.id) + "\nServer: " + str(message.guild)  + "\nChannel: " + str(message.channel) + "\n")
+"""
 
 client.run(os.getenv('BOT_TOKEN'))
