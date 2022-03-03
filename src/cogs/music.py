7e54282b0545c6ece3a6411bd63a8a6e3d4e990b
Yashvardhan Baid <codechaser2002@gmail.com>
2022-03-03 23:51:18 +0530

    ;-;

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -1,51 +1,143 @@
 import discord
 import youtube_dl
 import ffmpeg
-import wavelink
 from typing import Optional
 from datetime import datetime
 from discord import Embed, Member, Guild
-from discord.ext import commands
 from discord.ext.commands import Cog
 from discord.ext.commands import command
 from youtube_dl import YoutubeDL
 
-class Music(commands.Cog):
-    """Music cog to hold Wavelink related commands and listeners."""
-    
-    # copied from: https://github.com/PythonistaGuild/Wavelink#getting-started
-
-    def __init__(self, bot: commands.Bot):
+class Music(Cog):
+    def __init__(self, bot):
         self.bot = bot
+        self.is_playing = False
+        self.music_queue=[]
+        self.bot_voice_client = None
+        self.players = {}
+        self.ydl_options={
+            'format':'bestaudio',
+            'noplaylist':True,
+            'extractaudio': True,
+            'audioformat': 'mp3',
+            'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+            'restrictfilenames': True,
+            'nocheckcertificate': True,
+            'ignoreerrors': False,
+            'logtostderr': False,
+            'quiet': True,
+            'no_warnings': True,
+            'default_search': 'auto',
+            'source_address': '0.0.0.0'
+        }
+        self.ffmpeg_options={'before_options':'-reconnect 1 -reconnect_streamed 1 - reconnect_delay_max 5', 'options':'-vn'}
 
-        bot.loop.create_task(self.connect_nodes())
-
-    async def connect_nodes(self):
-        """Connect to our Lavalink nodes."""
-        await self.bot.wait_until_ready()
-
-        await wavelink.NodePool.create_node(bot=bot,
-                                            host='0.0.0.0',
-                                            port=2333,
-                                            password='YOUR_LAVALINK_PASSWORD')
-
-    @commands.Cog.listener()
-    async def on_wavelink_node_ready(self, node: wavelink.Node):
-        """Event fired when a node has finished connecting."""
-        print(f'Node: <{node.identifier}> is ready!')
-
-    @commands.command()
-    async def play(self, ctx: commands.Context, *, search: wavelink.YouTubeTrack):
-        """Play a song with the given search query.
-
-        If not connected, connect to our voice channel.
-        """
-        if not ctx.voice_client:
-            vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+    @command(name="join", aliases=["connect"], help = "joins the vc of the command author")
+    async def join_vc(self, ctx):
+        if ctx.author.voice is None:
+            embed=Embed(
+                title="Error",
+                description="User is not in a voice channel",
+                colour=0xff0000,
+                timestamp=datetime.utcnow())
+            await ctx.send(embed=embed)
+            return False
         else:
-            vc: wavelink.Player = ctx.voice_client
+            voice_channel = ctx.author.voice.channel
+            if ctx.voice_client is None:
+                await voice_channel.connect()
+                self.bot_voice_client = ctx.voice_client
+            else:
+                await ctx.voice_client.move_to(voice_channel)
+                self.bot_voice_client = ctx.voice_client
+            return True
 
-        await vc.play(search)
+    @command(name="leave", aliases=["disconnect"], help="leaves if connected to any vc")
+    async def leave_vc(self, ctx):
+        if ctx.guild.voice_client is None:
+            embed=Embed(
+                title="Error",
+                description="Dex is not in any voice channel",
+                colour=0xff0000,
+                timestamp=datetime.utcnow())
+            await ctx.send(embed=embed)
+        else:
+            await ctx.guild.voice_client.disconnect()
+            self.bot_voice_client = None
+
+    def search_yt(self, item):
+        with YoutubeDL(self.ydl_options) as ydl:
+            try:
+                info = ydl.extract_info("ytsearch:%s" % item,download = False)['entries'][0]
+            except Exception:
+                return False
+        return {'source':info['formats'][0]['url'], 'title':info['title']}
+        
+    def play_next(self,ctx):
+        if len(self.music_queue) > 0:
+            self.is_playing = True
+            m_url = self.music_queue[0][0]['source']
+            self.bot_voice_client = ctx.voice_client
+            self.music_queue.pop(0)
+            source = discord.FFmpegOpusAudio(m_url,**self.ffmpeg_options)
+            ctx.voice_client.play(source, after=lambda e: self.play_next(ctx))
+        else:
+            self.is_playing = False
+
+    async def play_music(self,ctx):
+        if len(self.music_queue) > 0:
+            self.is_playing = True
+            m_url = self.music_queue[0][0]['source']
+            if ctx.guild.voice_client is None:
+                await self.music_queue[0][1].connect()
+            elif ctx.guild.voice_client.channel != self.music_queue[0][1]:
+                ctx.voice_client.move_to(self.music_queue[0][1])
+            self.bot_voice_client = ctx.voice_client
+            self.music_queue.pop(0)
+            source = discord.FFmpegOpusAudio(m_url,**self.ffmpeg_options)
+            ctx.voice_client.play(source, after=lambda e: self.play_next(ctx))
+        else:
+            self.is_playing = False
+
+    @command(name="play", aliases=["p"], help = "apparently, replit won't let it play!")
+    async def play(self, ctx, keyword):
+        # embed = Embed(
+        #         title="Status",
+        #         colour=0xff0000,
+        #         timestamp=datetime.utcnow()
+        #     )
+        # embed.add_field(name="Error",value="Replit's not letting me play! T_T",inline=False)
+        # 
+        # await ctx.send(embed=embed)
+        # return
+        await self.join_vc(ctx)
+        if ctx.author.voice is None:
+            return
+        song = self.search_yt(keyword)
+        if type(song) == type(False):
+            embed = Embed(
+                title="Status",
+                colour=0xff0000,
+                timestamp=datetime.utcnow()
+            )
+            embed.add_field(name="Error",value="Failed to query YouTube for the music",inline=False)
+            embed.set_footer(text="Might be due to a livestream or a playlist")
+            await ctx.send(embed=embed)
+        else:
+            embed = Embed(
+                title="Status",
+                colour=0x00ff00,
+                timestamp=datetime.utcnow()
+            )
+            embed.add_field(name="Done",value="Song added to the queue",inline=False)
+            self.music_queue.append([song,ctx.author.voice.channel,ctx.author])
+            await ctx.send(embed=embed)
+            player = await ctx.voice_client.create_ytdl_player(song['source'])
+            self.players[ctx.guild.id] = player
+            player.start()
+            # if not self.is_playing:
+            #     await self.play_music(ctx)
+        
 
 def setup(bot):
     bot.add_cog(Music(bot))
