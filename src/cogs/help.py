4b9d94671950647b7e6bc7484ff6cc7bea2947c5
Yashvardhan Baid <codechaser2002@gmail.com>
2022-07-10 12:11:46 +0530

    paginated help menu

--- src/cogs/help.py
+++ src/cogs/help.py
@@ -1,7 +1,7 @@
 import discord
 from datetime import datetime
 from typing import Optional
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 
 class Help(commands.Cog):
@@ -54,15 +54,126 @@ class Help(commands.Cog):
             "ping": ["Shows the latency of the bot", ["latency"]]
         },
         "Help": {
-            "help": ["Shows this message", []]
+            "help": ["Shows description of commands", []],
+            "user-manual": ["Shows paginated description of commands", ["paginated-help", "help-pages"]],
         }
     }
+    reactions = {
+        "first": "⏮",
+        "back": "◀️",
+        "forward": "▶️",
+        "last": "⏭"
+    }
+    # ----------------------------------------------------------------------------------------------------------------------
 
     def __init__(self, bot):
         self.bot = bot
+        self.embeds_list = []
+        for category, commands_dict in self.commands_description.items():
+            self.embeds_list.append(discord.Embed(
+                title="Help: " + category,
+                description="",
+                color=self.bot.DEX_YELLOW,
+                timestamp=datetime.utcnow(),
+            ))
+            for command, description in commands_dict.items():
+                self.embeds_list[-1].add_field(name="`"+command +
+                                               "`", value=description[0], inline=False)
+            self.embeds_list[-1].set_footer(text="Page " + str(
+                len(self.embeds_list)) + " of " + str(len(self.commands_description)))
     # ----------------------------------------------------------------------------------------------------------------------
 
-    async def show_help(self, ctx):
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        reaction = str(payload.emoji)
+        user = self.bot.get_user(payload.user_id)
+        channel = self.bot.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        print("\nreaction added to a message authored by " + str(message.author) + "\n")
+        if user == self.bot.user:
+            return
+        if message.author != self.bot.user:
+            return
+        if len(message.embeds) == 0:
+            return
+        if (not message.embeds[0].title.startswith("Help: ")) or (message.embeds[0].title == "Help: All"):
+            return
+        page = int(message.embeds[0].footer.text.split(" ")[-3]) - 1
+        print("reaction: " + str(reaction) + "\n")
+        print("self.reactions: " + str(self.reactions) + "\n")
+        if reaction == self.reactions["first"]:
+            page = 0
+        elif reaction == self.reactions["back"]:
+            page -= 1
+        elif reaction == self.reactions["forward"]:
+            page += 1
+        elif reaction == self.reactions["last"]:
+            page = len(self.embeds_list) - 1
+        page %= len(self.embeds_list)
+        self.embeds_list[page].set_author(icon_url=user.avatar_url, name="|  " +
+                                          self.bot.DATABASE['guilds'][str(message.guild.id)]['prefix'] + "user-manual")
+        await message.edit(embed=self.embeds_list[page])
+        return
+    # ----------------------------------------------------------------------------------------------------------------------
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        reaction = str(payload.emoji)
+        user = self.bot.get_user(payload.user_id)
+        channel = self.bot.get_channel(payload.channel_id)
+        message = await channel.fetch_message(payload.message_id)
+        print("\nreaction removed from a message authored by " + str(message.author) + "\n")
+        if user == self.bot.user:
+            return
+        if message.author != self.bot.user:
+            return
+        if len(message.embeds) == 0:
+            return
+        if (not message.embeds[0].title.startswith("Help: ")) or (message.embeds[0].title == "Help: All"):
+            return
+        page = int(message.embeds[0].footer.text.split(" ")[-3]) - 1
+        print("reaction: " + str(reaction) + "\n")
+        print("self.reactions: " + str(self.reactions) + "\n")
+        if reaction == self.reactions["first"]:
+            page = 0
+        elif reaction == self.reactions["back"]:
+            page -= 1
+        elif reaction == self.reactions["forward"]:
+            page += 1
+        elif reaction == self.reactions["last"]:
+            page = len(self.embeds_list) - 1
+        page %= len(self.embeds_list)
+        self.embeds_list[page].set_author(icon_url=user.avatar_url, name="|  " +
+                                          self.bot.DATABASE['guilds'][str(message.guild.id)]['prefix'] + "user-manual")
+        await message.edit(embed=self.embeds_list[page])
+        return
+    # ----------------------------------------------------------------------------------------------------------------------
+
+    @commands.command(name="user-manual", aliases=["paginated-help", "help-pages"], help="Shows paginated description of commands")
+    async def user_manual_command(self, ctx, page: Optional[int] = 1):
+        if page is None:
+            page = 1
+        elif (page < 1) or (page > len(self.embeds_list)):
+            async with ctx.typing():
+                embed = discord.Embed(
+                    title="Error",
+                    description="Invalid page number!\nPage number must be between 1 & " + str(len(self.embeds_list)),
+                    color=0xFF0000,
+                    timestamp=datetime.utcnow(),
+                )
+            await ctx.reply(embed=embed)
+            return
+        page -= 1
+        async with ctx.typing():
+            self.embeds_list[page].set_author(icon_url=ctx.author.avatar_url, name="|  " +
+                                          self.bot.DATABASE['guilds'][str(ctx.guild.id)]['prefix'] + "user-manual")
+        msg = await ctx.reply(embed=self.embeds_list[page])
+        for name, reaction in self.reactions.items():
+            await msg.add_reaction(reaction)
+        return
+    # ----------------------------------------------------------------------------------------------------------------------
+
+    async def all_help(self, ctx):
         async with ctx.typing():
             embed = discord.Embed(
                 title="Help: All",
@@ -70,6 +181,7 @@ class Help(commands.Cog):
                 color=self.bot.DEX_YELLOW,
                 timestamp=datetime.utcnow(),
             )
+
             for category in self.commands_description.keys():
                 embed.add_field(name='**'+category+'**', value="\n".join(["**`{}`** - *{}*".format(
                     command, description[0]) for command, description in self.commands_description[category].items()]), inline=False)
@@ -80,11 +192,12 @@ class Help(commands.Cog):
                              self.bot.DATABASE['guilds'][str(ctx.guild.id)]['prefix'] + "help")
         await ctx.reply(embed=embed)
         return
+    # ----------------------------------------------------------------------------------------------------------------------
 
-    @commands.command(name="help", aliases=["?"], help="shows the list of valid commands")
+    @commands.command(name="help", aliases=["?"], help="Shows the list of valid commands")
     async def help_command(self, ctx, command_name: Optional[str]):
         if command_name is None:
-            await self.show_help(ctx)
+            await self.all_help(ctx)
             return
         invalid = True
         category = None
@@ -133,7 +246,6 @@ class Help(commands.Cog):
                              self.bot.DATABASE['guilds'][str(ctx.guild.id)]['prefix'] + "help " + command_name)
         await ctx.reply(embed=embed)
         return
-
     # ----------------------------------------------------------------------------------------------------------------------
 
 
